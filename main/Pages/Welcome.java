9f4b4a69cc08688735228ae86ad3a4ce4b668cb6
oscarqjh <oscar.jh9@gmail.com>
2023-03-18 12:56:00 +0800

    fix

--- main/Pages/Welcome.java
+++ main/Pages/Welcome.java
@@ -1,6 +1,9 @@
 package main.Pages;
 
+import java.io.IOException;
 import java.util.Scanner;
+
+import main.Utils.ClearConsole;
 
 public class Welcome extends Page{
     Scanner sc = new Scanner(System.in);
@@ -14,14 +17,24 @@ public class Welcome extends Page{
     @Override
     public Page executable(){
         
-        System.out.println("Welcome to FYP Interface");
-        System.out.println("Select user type:");
-        System.out.println("1) Student");
-        System.out.println("2) Staff");
-        System.out.println("Empty input to exit");
+        // print menu
+        ClearConsole.clearScreen();
+        System.out.println("╔══════════════════════════════════════════════════════════╗");
+        System.out.println("║ ███╗   ██╗████████╗██╗   ██╗███████╗██╗   ██╗██████╗ ██╗ ║");
+        System.out.println("║ ████╗  ██║╚══██╔══╝██║   ██║██╔════╝╚██╗ ██╔╝██╔══██╗██║ ║");
+        System.out.println("║ ██╔██╗ ██║   ██║   ██║   ██║█████╗   ╚████╔╝ ██████╔╝██║ ║");
+        System.out.println("║ ██║╚██╗██║   ██║   ██║   ██║██╔══╝    ╚██╔╝  ██╔═══╝ ██║ ║");
+        System.out.println("║ ██║ ╚████║   ██║   ╚██████╔╝██║        ██║   ██║     ██║ ║");
+        System.out.println("║ ╚═╝  ╚═══╝   ╚═╝    ╚═════╝ ╚═╝        ╚═╝   ╚═╝     ╚═╝ ║");
+        System.out.println("╠══════════════════════════════════════════════════════════╣");
+        System.out.println("║                      -User Selection-                    ║");
+        System.out.println("╠══════════════════════════════════════════════════════════╣");
+        System.out.println("║[1] Student                                               ║");
+        System.out.println("║[2] Staff                                                 ║");
+        System.out.println("╚══════════════════════════════════════════════════════════╝");
 
         // get user type
-        System.out.print("Enter your user type: ");
+        System.out.print("Enter your user type(empty input to exit): ");
         this.userType = sc.nextLine().trim();
         System.out.println();
 
---
+++ main/Utils/ClearConsole.java
@@ -0,0 +1,9 @@
+package main.Utils;
+
+
+public class ClearConsole {
+    public static void clearScreen(){  
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    } 
+}
