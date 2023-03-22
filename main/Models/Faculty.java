576501a8a8d28d480e003ca1201938df3980291a
ieatsighpies <76864712+ieatsighpies@users.noreply.github.com>
2023-03-22 15:21:22 +0800

    modified:   main/Models/Faculty.java commit changes

--- main/Models/Faculty.java
+++ main/Models/Faculty.java
@@ -1,31 +1,31 @@
-// package main.Models;
+package main.Models;
 
-// public class Faculty extends User {
-// 	private project[] project ;
-// 	private boolean isCoordinator ;
-// 	private int type ;
-	
-// 	public Faculty(String name, String email, boolean isCoordinator, Project[] project) {
-//         super(name, email);
-//         this.isCoordinator = isCoordinator;
-//         this.project = project;
-//     }
+public class Faculty extends User {
+	private project[] project;
+	private boolean isCoordinator;
+	private int type;
 
-//     public String getName() {
-//         return super.name;
-//     }
+	public Faculty(String name, String email, boolean isCoordinator, Project[] project) {
+        super(name, email);
+        this.isCoordinator = isCoordinator;
+        this.project = project;
+    }
 
-//     public String getEmail() {
-//         return super.email;
-//     }
+    public String getName() {
+        return super.name;
+    }
 
-//     public String getUserID() {
-//         return super.userID;
-//     }
+    public String getEmail() {
+        return super.email;
+    }
 
-//     public Project getProject(int projectID) {
-//         return this.project[projectID];
-//     }
-// }
+    public String getUserID() {
+        return super.userID;
+    }
+
+    public Project getProject(int projectID) {
+        return this.project[projectID];
+    }
+}
 
 
