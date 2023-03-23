122590b99d091449dd5ff42a3cdcb341d4a157c2
ieatsighpies <76864712+ieatsighpies@users.noreply.github.com>
2023-03-23 21:04:26 +0800

    yes

--- main/Models/Coordinator.java
+++ main/Models/Coordinator.java
@@ -1,6 +1,6 @@
 package main.Models;
 
-public class Coordinator extends Faculty{
+public class Coordinator extends Supervisor{
 
     public Coordinator(String name, String email){
         super(name, email);
