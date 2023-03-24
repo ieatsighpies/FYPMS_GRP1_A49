821c6f2e6d019a3575b1a977a91f650b85b361db
oscarqjh <oscar.jh9@gmail.com>
2023-03-24 16:39:01 +0800

    edited data file

--- main/script.py
+++ main/script.py
@@ -5,6 +5,7 @@ with open('Data\project_record.csv', 'r') as istr1:
             with open('Data\\faculty_list.csv', 'r') as istr2:
                 for line2 in istr2:
                     data2 = line2.split(",")
-                    if(data1[0] == data2[0]):
-                            line = data1[0]+ ","+data2[2]+ ","+data1[1]
-                            print(line, file=ostr, end='')
+                    if(data1[1] == data2[0]):
+                            line = ",".join([data1[0],data2[0], data2[1], data1[3].rstrip(),"AVAILABLE,NaN,NaN"])
+                            line = line.rstrip()  # remove the newline character at the end
+                            print(line, file=ostr)
