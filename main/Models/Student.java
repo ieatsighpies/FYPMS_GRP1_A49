13ecfcbfef935ceaa7c3379567743cb49ff38d13
ieatsighpies <76864712+ieatsighpies@users.noreply.github.com>
2023-03-24 15:38:29 +0800

    modified:   main/Models/Student.java commit changes

--- main/Models/Student.java
+++ main/Models/Student.java
@@ -3,14 +3,12 @@ package main.Models;
 public class Student extends User {
     //student HAS-A project
     protected Project project;
-    private boolean assignStatus;
     //to differentiate from students without a project
     protected boolean deregistered;
 
     public Student(String name, String email){
         super(name, email);
         this.project = null;
-        this.assignStatus = false;
         this.deregistered = false;
     }
     public String getName(){
@@ -22,14 +20,12 @@ public class Student extends User {
     public String getUserID(){
         return userID;
     }
-    public boolean getAssignStatus(){
-        return assignStatus;
-    }
     public boolean getDeregisteredStatus(){
         return deregistered;
     }
     public Project getProject(){
-        return project;
+        if(this.project.status==projectStatus.ALLOCATED)
+            return project;
     }
 
     // public void setProject(Student s, Project project){
