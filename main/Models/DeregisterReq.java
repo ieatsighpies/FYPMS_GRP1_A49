3cff3cb143c80b6ea01c465d3b0e4755fff3320a
ieatsighpies <76864712+ieatsighpies@users.noreply.github.com>
2023-04-03 11:25:55 +0800

    Update DeregisterReq.java

--- main/Models/DeregisterReq.java
+++ main/Models/DeregisterReq.java
@@ -6,4 +6,74 @@ public class DeregisterReq extends Request {
                          requestStatus_ENUM status, String projectID, String comment) {
         super(requestID, requesterID, requesteeID, requestType, status, projectID, comment);
     }
+    
+    @Override
+    public String getComments() {
+        return super.getComments();
+    }
+
+    @Override
+    public String getProjectID() {
+        return super.getProjectID();
+    }
+
+    @Override
+    public String getRequestID() {
+        return super.getRequestID();
+    }
+
+    @Override
+    public requestStatus_ENUM getRequestStatus() {
+        return super.getRequestStatus();
+    }
+
+    @Override
+    public String getRequestType() {
+        return super.getRequestType();
+    }
+
+    @Override
+    public String getRequesteeID() {
+        return super.getRequesteeID();
+    }
+
+    @Override
+    public String getRequesterID() {
+        return super.getRequesterID();
+    }
+
+    @Override
+    public void setComments(String comments) {
+        super.setComments(comments);
+    }
+
+    @Override
+    public void setProjectID(String projectID) {
+        super.setProjectID(projectID);
+    }
+
+    @Override
+    public void setRequestID(String requestID) {
+        super.setRequestID(requestID);
+    }
+
+    @Override
+    public void setRequestStatus(requestStatus_ENUM requestStatus) {
+        super.setRequestStatus(requestStatus);
+    }
+
+    @Override
+    public void setRequestType(String requestType2) {
+        super.setRequestType(requestType2);
+    }
+
+    @Override
+    public void setRequesteeID(String requesteeID) {
+        super.setRequesteeID(requesteeID);
+    }
+
+    @Override
+    public void setRequesterID(String requesterID) {
+        super.setRequesterID(requesterID);
+    }
 }
