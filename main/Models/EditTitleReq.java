4fa466792e2b293575139114e1720b78c547d42d
Oscarqjh <aerialguard@gmail.com>
2023-04-06 22:39:43 +0800

    new

--- main/Models/EditTitleReq.java
+++ main/Models/EditTitleReq.java
@@ -17,4 +17,16 @@ public class EditTitleReq extends Request{
     public void setTitle(String newTitle) {
         this.newTitle = newTitle;
     }
+
+    @Override
+    public void printInfo() {
+        System.out.println("╔══════════════════════════════════════════════════════════╗");
+        System.out.printf("║ Request ID: \u001B[33m%-45s\u001B[0m║\n", this.getRequestID());
+        System.out.printf("║ Requester ID: \u001B[33m%-45s\u001B[0m║\n", this.getRequesterID());
+        System.out.printf("║ Requestee ID: \u001B[33m%-45s\u001B[0m║\n", this.getRequestID());
+        System.out.println("╠══════════════════════════════════════════════════════════╣"); 
+        System.out.printf("║ Current Title: \u001B[33m%-80s\u001B[0m║\n", this.getTitle());
+        System.out.printf("║ New Title: \u001B[33m%-45s\u001B[0m║\n", this.getRequestID());
+        System.out.println("╚══════════════════════════════════════════════════════════╝");
+    }
 }
