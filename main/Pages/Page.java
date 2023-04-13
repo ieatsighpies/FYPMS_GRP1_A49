a1e13adad8f24dfd9b44c55862753e4e6770bf5e
ieatsighpies <76864712+ieatsighpies@users.noreply.github.com>
2023-04-13 17:24:50 +0800

    update javadoc

--- main/Pages/Page.java
+++ main/Pages/Page.java
@@ -1,14 +1,33 @@
 package main.Pages;
+/**
+* Abstract class for pages. All pages will inherit Page
+*
+* @author Dr. Heinz Doofenshmirtz
+* @version 1.0
+* @since 2023-4-13
+*/
 
 public abstract class Page {
+    /**
+     * Previous page visited
+     */
     private Page previousPage;
-    
+    /**
+     * Constructor of Page
+     * @param previousPage previous page visited
+     */
     public Page(Page previousPage){
         this.previousPage = previousPage;
     }
-
+    /**
+     * main functionality of the page
+     * @return page
+     */
     public abstract Page executable();
-
+    /**
+     * Gets previous page visited
+     * @return previous page visited
+     */
     protected Page getPreviousPage() {
         if (this.previousPage != null)
             return this.previousPage;
