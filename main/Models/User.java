f9a09c68c8ef5b0f8a5edf1aadd6a6f1db7462af
ieatsighpies <76864712+ieatsighpies@users.noreply.github.com>
2023-04-13 16:50:28 +0800

    modified:   main/Models/Student.java 	modified:   main/Models/Supervisor.java 	modified:   main/Models/TransferStudentReq.java 	modified:   main/Models/User.java commit changes

--- main/Models/User.java
+++ main/Models/User.java
@@ -1,31 +1,66 @@
 package main.Models;
+/**
+* The user base class. Different user types will inherit this class
+*
+* @author Dr. Heinz Doofenshmirtz
+* @version 1.0
+* @since 2023-4-13
+*/
 
 public class User {
+    /**
+     * Name of user
+     */
     protected String name;
+    /**
+     * Email of user
+     */
     protected String email;
+    /**
+     * ID of user
+     */
     protected String userID;
+    /**
+     * type of user
+     */
     protected int type;
 
-    
 
+    /**
+     * Constructor of User
+     * @param name user's name
+     * @param email user's email
+     */
     public User(String name, String email){
         this.name = name;
         this.email = email;
         this.userID = email.split("@")[0];
     }
-
+    /**
+     * Gets user's name
+     * @return user's name
+     */
     public String getName(){
         return this.name;
     }
-
+    /**
+     * Gets user's email
+     * @return user's email
+     */
     public String getEmail(){
         return this.email;
     }
-
+    /**
+     * Gets user's ID
+     * @return user's ID
+     */
     public String getUserID(){
         return this.userID;
     }
-
+    /**
+     * Gets type of user
+     * @return type of user
+     */
     public int getType() {
         return this.type;
     }
